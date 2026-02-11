f0ff54edbd47de4fc52593fa346a491dfb282a00
SONNAYITANKAM SRIVAIBHAV <139890217+SRIVAIBHAV25@users.noreply.github.com>
2026-02-11 10:40:49 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -66,9 +66,9 @@ export default function Login() {
 
   setLoading(true);
 
-  try {
+   try {
     const API = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
-const res = await fetch(`${API}/login`, {
+    const res = await fetch(`${API}/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
