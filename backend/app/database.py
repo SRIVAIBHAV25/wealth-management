91bad060150f90e3c915fd2373fa7b5999292c68
SONNAYITANKAM SRIVAIBHAV <139890217+SRIVAIBHAV25@users.noreply.github.com>
2026-02-11 09:19:47 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,7 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL", "psql 'postgresql://neondb_owner:npg_mn5hJ4quAOaN@ep-wandering-rain-a17vok4o-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_mn5hJ4quAOaN@ep-wandering-rain-a17vok4o-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
 
 engine = create_engine(
     DATABASE_URL,
